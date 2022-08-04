34bd97138ef2dcf57e8157d1b619ac7a16beb82e
BigRonnyColeman <71296085+BigRonnyColeman@users.noreply.github.com>
2022-08-04 17:33:56 +1000

    Revert "Revert "Merge branch 'master' of https://github.com/Amy-And-Co/ZEIT3101""
    This reverts commit 5b9928d703cd82d00f875fdb566cf433b3f929b4.

--- amyandco/lib/amyandco_web/live/public_room_live.ex
+++ amyandco/lib/amyandco_web/live/public_room_live.ex
@@ -2,6 +2,7 @@ defmodule AmyandcoWeb.PublicRoomLive do
   use AmyandcoWeb, :live_view
   require Logger
 
+
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
@@ -18,14 +19,17 @@ defmodule AmyandcoWeb.PublicRoomLive do
        message: "",
        messages: [],
        user_list: [],
-       temporary_assigns: [messages: []]
+       temporary_assigns: [messages: []],
+       chat_color: "background: rgb(238,174,202);background: linear-gradient(52deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);"
      )}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
     message = %{uuid: UUID.uuid4(), content: message, username: socket.assigns.username}
-    AmyandcoWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    if message.content != "" do
+      AmyandcoWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    end
     {:noreply, assign(socket, message: "")}
   end
 
@@ -57,15 +61,48 @@ defmodule AmyandcoWeb.PublicRoomLive do
     {:noreply, assign(socket, messages: join_messages ++ leave_messages, user_list: user_list)}
   end
 
-  def display_message(%{type: :system, uuid: uuid, content: content}) do
+  def display_message(%{type: :system, uuid: uuid, content: content}, yah: yah) do
     ~E"""
-    <p id="<%= uuid %>"><em><%= content %></em></p>
+    <p id="<%= uuid %>" style="color:black;""><em><%= content %></em></p>
     """
   end
 
-  def display_message(%{uuid: uuid, content: content, username: username}) do
+  def display_message(%{uuid: uuid, content: content, username: username}, yah: yah) do
+    if username == yah do
     ~E"""
-    <p id="<%= uuid %>"><strong><%= username %></strong>: <%= content %></p>
+    <div id="<%= uuid %><>4b"style="justify-content: right;display: flex;flex-direction:row-reverse;">
+    <p id="<%= uuid %>" style="text-align:right;display: inline-block;padding:10px 15px;background:rgba(173,216,230,0.8);margin:3px;border-radius:20px;">
+    <%= content %></p>
+    </div>
+    """
+    else
+    ~E"""
+    <div id="<%= uuid %><>4b"style="justify-content: left;display: flex;flex-direction:row;">
+    <p id="<%= uuid %>" style="text-align:left;display: inline-block;padding:10px 15px;background:rgba(238,174,202,0.8);margin:3px;border-radius:20px;"><strong><%= username %></strong>: <%= content %></p>
+    </div>
+    """
+    end
+  end
+
+  @impl true
+  def handle_event("change_color", %{"value" =>color}, socket) do
+    chosencolor = case color do
+       "style1" -> "background: rgb(238,174,202); background: linear-gradient(52deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);"
+       "style2" -> "background: rgb(34,193,195); background: linear-gradient(62deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%); "
+       "style3" -> "background: rgb(131,58,180); background: linear-gradient(31deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);"
+       "style4" -> " background: rgb(2,145,0); background: linear-gradient(31deg, rgba(2,145,0,1) 0%, rgba(167,253,29,0.6180672952774859) 50%, rgba(228,252,69,0.8953782196472339) 100%);"
+       "style5" -> " background: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);"
+       "style6" -> "background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;
+       background-blend-mode: multiply,multiply;"
+       _ -> "background: rgb(238,174,202); background: linear-gradient(52deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);"
+    end
+    {:noreply, assign(socket, chat_color: chosencolor)}
+  end
+
+  def display_chat_box(chat_color) do
+    ~E"""
+    <div id="chat-container" style="<%= chat_color %>">
     """
   end
+
 end
